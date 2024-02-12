ff5c91b59459750a42d694994c9e95dec329f59a
BugrinaA <tsaaeel@gmail.com>
2024-02-13 01:41:34 +0500

    Установлены пакеты NuGet для работы с БД + реализовано подключение к БД. Бугрина

--- Classes/Common/Connection.cs
+++ Classes/Common/Connection.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ComputerClubBugrina.Classes.Common
 {
-    class Connection
+    public class Connection
     {
+        public static readonly string connect = "server=127.0.0.1;port=3306;database=compclub;user=root;pwd=;SslMode=none;";
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(connect);
+            try
+            {
+                connection.Open();
+                return connection;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Ошибка подключения: " + ex.Message);
+                return null;
+            }
+        }
+
+        public static MySqlDataReader Query(string Sql, MySqlConnection connection)
+        {
+            MySqlCommand command = new MySqlCommand(Sql, connection);
+            return command.ExecuteReader();
+        }
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
     }
 }
